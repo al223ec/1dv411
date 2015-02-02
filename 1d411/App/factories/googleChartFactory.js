82653d4542846c8d2ebd14b9616102b1ae094a7a
th222fa <th222fa@student.lnu.se>
2015-02-02 22:49:39 +0100

    data hämtas nu från API:et

--- 1d411/App/factories/googleChartFactory.js
+++ 1d411/App/factories/googleChartFactory.js
@@ -1,31 +1,42 @@
 ﻿//load the chart library
 google.load('visualization', '1', { packages: ['corechart'] });
 
-angular.module("ChartProvider", [])
+angular.module("ChartProvider", ['AppService'])
 
-.factory("ChartGenerator",['AppService', function (AppService) {
+.factory("ChartGenerator", ['AppService', function (AppService) {
 
     return {
         draw: function () {
-            var data1 = {};
-            data1 = new google.visualization.arrayToDataTable([
-                ['Year', 'Sales', 'Expenses', 'Profit'],
-                ['2014', 1000, 400, 800],
-                ['2015', 1500, 650, 1300],
-                ['2016', 1200, 500, 1000],
-                ['2017', 600, 900, 500]
-            ]);
+            var diagramDataPromise = AppService.getDiagramData();
+            diagramDataPromise.success(function (json) {
+                var orderData = JSON.parse(JSON.stringify(json));
 
-            var options = {
-                title: 'Company Performance',
-                vAxis: { textPosition: 'none' },
-                enableInteractivity: false
-            };
+                var data = new google.visualization.DataTable();
 
-            var googleChart = new google.visualization.ColumnChart(document.getElementById("chartDiv"));
-            googleChart.draw(data1, options);
+                data.addColumn('string', 'Datum');
+                data.addColumn('number', 'Ordrar');
+                data.addColumn('number', 'Ordrar Förra Året')
+               
 
-        }
+                orderData.forEach(function (order) {
+                    console.log(order);
+                    data.addRows([[order.date, order.orders, order.ordersLastYear]])
+                });
+
+              
+               
+                var options = {
+                    title: 'Företagsdata',
+                    vAxis: { textPosition: 'none' },
+                    enableInteractivity: false
+                };
+
+                console.log(data);
+                var googleChart = new google.visualization.ColumnChart(document.getElementById("chartDiv"));
+                googleChart.draw(data, options);
+
+                })
+            }
     }
 
 }])
