58200c4e4e1976b3989606a93afb49916c37a8b7
SheriefBadran <sb222rf@student.lnu.se>
2015-02-10 21:26:29 +0100

    created test controller and directive.

---
+++ 1d411/App/controllers/TestScreenCtrl.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+var layoutCtrl = angular.module('TestScreen', []);
+
+layoutCtrl.controller('TestScreenCtrl', ['$scope', 'ScreenService', '$routeParams', function ($scope, ScreenService, $routeParams) {
+
+    console.log(google);
+    ScreenService.getLayout($routeParams.id)
+        .success(function (response) {
+            console.log(response);
+            $scope.layout = response;
+        });
+
+    ScreenService.getScreen()
+        .success(function (response) {
+            console.log(response);
+        });
+
+}]);
