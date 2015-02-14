7857aee01c9151f46f13bc826bc60267c5fe2840
al223ec <al223ec@student.lnu.se>
2015-02-14 16:35:21 +0100

    Comment

--- 1d411/App/app.js
+++ 1d411/App/app.js
@@ -56,6 +56,7 @@ app.config(['$httpProvider', function($httpProvider) {
 }]);
 
 app.constant("appConfig", {
+    //TODO:: Besluta om detta är smart
     "templateUrlRoot": "/Views/App/Templates/",
     "partialUrlRoot": "/Views/App/Partials/",
 })
