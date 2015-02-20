c7ef83abd857bf54113b101fc4a8ebbe34c43fd8
al223ec <al223ec@student.lnu.se>
2015-02-20 17:18:17 +0100

    Fixat partial sortering

--- 1d411/App/controllers/ScreenCtrl.js
+++ 1d411/App/controllers/ScreenCtrl.js
@@ -3,6 +3,7 @@ var screenModule = angular.module('Screen', []);
 
 screenModule.controller('ScreenController', ['$scope', 'LayoutScreenService', '$routeParams', 'appConfig', '$timeout',
     function ($scope, LayoutScreenService, $routeParams, appConfig, $timeout) {
+ 
         LayoutScreenService.getScreen($routeParams.id)
             .error(function (data, status, headers, config) {
 
@@ -10,26 +11,29 @@ screenModule.controller('ScreenController', ['$scope', 'LayoutScreenService', '$
             .success(function (screenData, status, headers, config) {
                 if (screenData) { //I dagsläget returnerar servern en 200 även om id inte finns i databasen men data är däremot null
                     $scope.screen = screenData;
-                    LayoutScreenService.getPagesWithScreenId($scope.screen.id).success(function (pagesData) {
+
+                    LayoutScreenService.getPagesWithScreenId($scope.screen.id)
+                        .success(function (pagesData) {
                         
-                        // MY CODE!
                         $scope.pages = pagesData;
 
-                        var current = 0;
+                        
                         var nextSlide = function (page) {
-                            console.log(page);
                             $scope.page = page;
                             var templateName = page.template.fileName == null ? "default_template" : page.template.fileName;
                             var templateUrl = appConfig.templateUrlRoot + templateName + ".html";
                             $scope.pageTemplate = templateUrl;
-                            $scope.partials = page.partials;
 
-                            //$scope.currentIndex = ($scope.currentIndex < data.length - 0) ? ++$scope.currentIndex : 0;
-                            //$timeout(nextSlide, 1000);
+                            var sortedPartials = []; 
+                            for (var i = 0; i < page.partials.length; i++) {
+                                sortedPartials[page.partials[i].position] = page.partials[i];
+                            }
+
+                            $scope.partials = sortedPartials;
                         }
 
-                        // timeout-method: refresha varje "screen.timeout" sekunder
-                        // use screen.timeout instead of 3000.
+                        var current = 0;
+                        nextSlide($scope.pages[current]);
                         var timer;
                         var sliderFunc = function () {
                             timer = $timeout(function () {
@@ -44,23 +48,7 @@ screenModule.controller('ScreenController', ['$scope', 'LayoutScreenService', '$
                         $scope.$on('$destroy', function () {
                             $timeout.cancel(timer);
                         });
-
-
-
-
                     });
-                    /*
-                    var templateName = data.templateUrl == null ? "default_template" : data.templateUrl;
-                    $scope.templateUrl = appConfig.templateUrlRoot + templateName + ".html";
-
-                    var partials = data.partials;
-                    var sortedPartials = [];
-
-                    for (var i = 0; i < partials.length; i++) {
-                        sortedPartials[partials[i].position] = partials[i]; 
-                    }
-                    $scope.partials = sortedPartials;
-                    */
                 } else {
                     console.log("Verkar inte få något bra svar från servern, är databasen seedad och uppe? Annars kanske inte id:et finns")
                 }
