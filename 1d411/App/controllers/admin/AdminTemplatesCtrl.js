5e288e9f5ff5387fbb4986de324426784e801ced
SheriefBadran <sb222rf@student.lnu.se>
2015-03-03 15:31:18 +0100

    transfer code from branch

--- 1d411/App/controllers/admin/AdminTemplatesCtrl.js
+++ 1d411/App/controllers/admin/AdminTemplatesCtrl.js
@@ -4,4 +4,24 @@ var adminModule = angular.module('AdminTemplates', []);
 adminModule.controller('AdminTemplatesController', ['$scope', 'LayoutScreenService', '$routeParams', 'appConfig',
     function ($scope, LayoutScreenService, $routeParams, appConfig) {
 
+        var vm = this;
+
+        LayoutScreenService.getTemplates().success(function (data) {
+
+            vm.templates = data;
+        });
+
+        vm.validateTemplateName = function (name) {
+
+            vm.templates.filter(function (template) {
+
+                console.log(!name === template.name);
+            });
+        };
+
+        vm.postTemplate = function (template, form) {
+
+            console.log(template);
+            form.$setPristine();
+        };
 }]);
