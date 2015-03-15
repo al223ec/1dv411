f5a4730f6ade2b026c346c41fc585bd3d5df7fcb
afrxx09 <afrxx09@student.lnu.se>
2015-03-15 14:57:19 +0100

    Bug-fix: Växla page på screen

--- 1d411/App/controllers/ScreenCtrl.js
+++ 1d411/App/controllers/ScreenCtrl.js
@@ -30,14 +30,13 @@ screenModule.controller('ScreenController', ['$scope', 'LayoutScreenService', '$
             var current = 0;
             nextSlide($scope.pages[current]);
 
-
             var sliderFunc = function () {
                 timer = $timeout(function () {
                     nextSlide($scope.pages[current]);
                     current = (current + 1 >= $scope.pages.length) ? 0 : current + 1;
 
-                    timer = $timeout(sliderFunc, $scope.pages[current].timer);
-                }, $scope.pages[current].timer);
+                    timer = $timeout(sliderFunc, $scope.screen.timer);
+                }, $scope.screen.timer);
             };
             sliderFunc();
             var timer;
