625ad458d16f82ceaaf4b27b2dfe4342c634a5f5
afrxx09 <afrxx09@student.lnu.se>
2015-03-15 20:32:15 +0100

    Templates och ny Seed

--- 1dv411.Domain/Migrations/Configuration.cs
+++ 1dv411.Domain/Migrations/Configuration.cs
@@ -9,6 +9,10 @@ namespace _1dv411.Domain.Migrations
 
     internal sealed class Configuration : DbMigrationsConfiguration<_1dv411.Domain.DAL.ApplicationContext>
     {
+        private IList<Template> _templates;
+        private IList<Page> _pages;
+        private IList<Screen> _screens;
+        private IList<PageScreen> _pageScreens;
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
@@ -17,73 +21,125 @@ namespace _1dv411.Domain.Migrations
 
         protected override void Seed(_1dv411.Domain.DAL.ApplicationContext context)
         {
-            /*
-                        * TODO: Fixa mer och bättre testdata 
-                        * kommentera bort detta som default, kör endast en gång 
-            
-            
-            var hero = SeedHero(context);
-            var def = SeedDefault(context);
-            var hor = SeedHorizontal(context);
+            SeedTemplates(context);
+            SeedScreens(context);
+            SeedPages(context);
+            SeedPagePartials(context);
+            SeedPageScreens(context);
 
-            Screen screen = new Screen
-            {
-                 Name = "Lager"
-            };
-            PageScreen pageScreenHero = new PageScreen
-            {
-                Page = hero,
-                Screen = screen,
-            };
-            PageScreen pageScreenDef = new PageScreen
-            {
-                Page = def,
-                Screen = screen,
-            };
-            Screen fika = new Screen
-            {
-                Name = "Fika"
-            };
-            PageScreen pageScreenHor = new PageScreen
-            {
-                Page = hor,
-                Screen = fika,
-            };
-
-            context.PageScreens.Add(pageScreenHor);
-            context.PageScreens.Add(pageScreenHero);
-            context.PageScreens.Add(pageScreenDef);
-            context.SaveChanges();
-            
-            /**** För att seeda ordrar och shipments
+            /*För att seeda ordrar och shipments*/
             var ordersThisYear = GetTestOrders(DateTime.Today);
             ordersThisYear.ForEach(o => context.Orders.AddOrUpdate(o));
             var ordersLastYear = GetTestOrders(DateTime.Today.AddYears(-1));
             ordersLastYear.ForEach(o => context.Orders.AddOrUpdate(o));
-
             var shipmentsThisYear = GetTestShipments(DateTime.Today);
             shipmentsThisYear.ForEach(s => context.Shipments.AddOrUpdate(s));
             var shipmentsLastYear = GetTestShipments(DateTime.Today.AddYears(-1));
             shipmentsLastYear.ForEach(s => context.Shipments.AddOrUpdate(s));
-            
             context.SaveChanges(); 
-            /**/
         }
 
-        private Page CreatePage(string name, string templateFileName, int numberOfPartials)
+        private void SeedTemplates(_1dv411.Domain.DAL.ApplicationContext context)
         {
-            Template template = new Template
-            {
-                Name = string.Format("Template name for {0}", templateFileName),
-                FileName = templateFileName,
-                NumberOfPartials = numberOfPartials
-            }; 
-            Page page = new Page
-            {
-                Name = name,
-                Template = template,
+            _templates = new List<Template>{
+                new Template{Name = "default", FileName = "default.html", NumberOfPartials = 1},
+                new Template{Name = "Horizontal 1", FileName = "horizontal_1.html", NumberOfPartials = 2},
+                new Template{Name = "Horizontal 2", FileName = "horizontal_2.html", NumberOfPartials = 3},
+                new Template{Name = "Horizontal 3", FileName = "horizontal_3.html", NumberOfPartials = 2},
+                new Template{Name = "Horizontal 4", FileName = "horizontal_4.html", NumberOfPartials = 2},
+                new Template{Name = "Horizontal 5", FileName = "horizontal_5.html", NumberOfPartials = 3},
+                new Template{Name = "Mixed 1", FileName = "mixed_1.html", NumberOfPartials = 3},
+                new Template{Name = "Mixed 2", FileName = "mixed_2.html", NumberOfPartials = 3},
+                new Template{Name = "Mixed 3", FileName = "mixed_3.html", NumberOfPartials = 4},
+                new Template{Name = "Mixed 4", FileName = "mixed_4.html", NumberOfPartials = 4},
+                new Template{Name = "Mixed 5", FileName = "mixed_5.html", NumberOfPartials = 4},
+                new Template{Name = "Vertical 1", FileName = "vertical_1.html", NumberOfPartials = 2},
+                new Template{Name = "Vertical 2", FileName = "vertical_2.html", NumberOfPartials = 2},
+                new Template{Name = "Vertical 3", FileName = "vertical_3.html", NumberOfPartials = 2}
             };
-            return page;
+            foreach(Template t in _templates){
+                context.Templates.AddOrUpdate(t);
+            }
+            context.SaveChanges();
+        }
+
+        private void SeedPages(_1dv411.Domain.DAL.ApplicationContext context)
+        {
+            _pages = new List<Page> {
+                new Page{Name = "Page 1", Template = _templates[0]},
+                new Page{Name = "Page 2", Template = _templates[2]},
+                new Page{Name = "Page 3", Template = _templates[6]},
+                new Page{Name = "Page 4", Template = _templates[12]}
+            };
+            foreach (Page p in _pages)
+            {
+                context.Pages.AddOrUpdate(p);
+            }
+            context.SaveChanges();
+        }
+
+        private void SeedPagePartials(DAL.ApplicationContext context)
+        {
+            Page p1 = _pages[0];
+            p1.Partials = new List<Partial>{
+                CreateDiagram (p1, 1, DiagramType.WeeklyOrders)
+            };
+            Page p2 = _pages[1];
+            p2.Partials = new List<Partial>{
+                CreateText(p2, 1, "Example Text"),
+                CreateDiagram (p2, 2, DiagramType.WeeklyOrders),
+                CreateImage(p2, 3, "http://i.imgur.com/ReWjLO1.jpg"),
+            };
+            Page p3 = _pages[2];
+            p3.Partials = new List<Partial>
+            {
+                CreateText(p3, 1, "Example Text"),
+                CreateDiagram (p3, 2, DiagramType.WeeklyOrders),
+                CreateImage(p3, 3, "http://i.imgur.com/ReWjLO1.jpg"),
+            };
+            Page p4 = _pages[3];
+            p4.Partials = new List<Partial>
+            {
+                CreateText(p4, 1, "Example Text"),
+                CreateText(p4, 2, "Example Text")
+            };
+            context.Pages.AddOrUpdate(p1);
+            context.Pages.AddOrUpdate(p2);
+            context.Pages.AddOrUpdate(p3);
+            context.Pages.AddOrUpdate(p4);
+            context.SaveChanges();
+        }
+
+        private void SeedScreens(_1dv411.Domain.DAL.ApplicationContext context)
+        {
+            _screens = new List<Screen> {
+                new Screen{Name = "Scrren 1", Timer = 5000},
+                new Screen{Name = "Scrren 2", Timer = 6000},
+                new Screen{Name = "Scrren 3", Timer = 7000}
+            };
+            foreach (Screen s in _screens)
+            {
+                context.Screens.AddOrUpdate(s);
+            }
+            context.SaveChanges();
+        }
+
+        private void SeedPageScreens(DAL.ApplicationContext context)
+        {
+           _pageScreens = new List<PageScreen>{
+                new PageScreen { Screen = _screens[0], Page = _pages[0] },
+                new PageScreen { Screen = _screens[0], Page = _pages[1] },
+                new PageScreen { Screen = _screens[1], Page = _pages[3] },
+                new PageScreen { Screen = _screens[1], Page = _pages[2] },
+                new PageScreen { Screen = _screens[2], Page = _pages[1] },
+                new PageScreen { Screen = _screens[2], Page = _pages[2] },
+                new PageScreen { Screen = _screens[2], Page = _pages[3] }
+            };
+            foreach (PageScreen ps in _pageScreens)
+            {
+                context.PageScreens.AddOrUpdate(ps);
+            }
+            context.SaveChanges();
         }
 
         private Text CreateText(Page page, int position, string content)
@@ -108,7 +164,7 @@ namespace _1dv411.Domain.Migrations
 
         private Image CreateImage(Page page, int position, string filename = null)
         {
-            var url = string.Format("/Views/App/Partials/Images/{0}", filename == null ? "" : filename); 
+            var url = string.Format("{0}", filename == null ? "" : filename);
             return new Image
             {
                 Page = page,
@@ -116,57 +172,9 @@ namespace _1dv411.Domain.Migrations
                 Url = url
             };
         }
-        private Page SeedHero(DAL.ApplicationContext context)
-        {
-            Page hero = CreatePage("Hero", "hero.html", 3);
 
-            var partials = new List<Partial>
-            {
-                CreateText(hero, 1, "Hero heading"),
-                CreateDiagram (hero, 2, DiagramType.WeeklyOrders),
-                CreateImage(hero, 3, "dodge.jpg"),
-            };
 
-            hero.Partials = partials;
-            return AddOrUpdatePage(context, hero);
-
-        }
-        private Page SeedDefault(DAL.ApplicationContext context)
-        {
-            Page defaultPage = CreatePage("Default page", "default_template.html", 2);
-            var partials = new List<Partial>
-            {
-                CreateText(defaultPage, 1, "Default page text"),
-                CreateDiagram (defaultPage, 2, DiagramType.WeeklyOrders),
-            };
-
-            defaultPage.Partials = partials;
-            return AddOrUpdatePage(context, defaultPage);
-        }
-        private void SeedVertical(DAL.ApplicationContext context)
-        {
-            throw new NotImplementedException();
-        }
-        private Page SeedHorizontal(DAL.ApplicationContext context)
-        {
-            Page horizontal = CreatePage("Horizontal page", "horizontal.html", 3);
-
-            var partials = new List<Partial>
-            {
-                CreateDiagram (horizontal, 1, DiagramType.WeeklyOrders),
-                CreateDiagram (horizontal, 2, DiagramType.MonthlyOrders),
-                CreateDiagram (horizontal, 3, DiagramType.YearlyOrders),
-            };
-
-            horizontal.Partials = partials;
-            return AddOrUpdatePage(context, horizontal);
-        }
-        private Page AddOrUpdatePage(DAL.ApplicationContext context, Page page)
-        {
-            context.Pages.AddOrUpdate(l => l.Name, page);
-            context.SaveChanges();
-            return page;
-        }
+        /* Seed statistics methods */
         private List<Order> GetTestOrders(DateTime date)
         {
             Random rnd = new Random();
