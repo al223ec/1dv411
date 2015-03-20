24e3abb016dc81065576d77c052e11fed8c7326e
afrxx09 <afrxx09@student.lnu.se>
2015-03-20 01:00:41 +0100

    Buggfix, visade först sida två gånger i rad

--- 1d411/App/controllers/ScreenCtrl.js
+++ 1d411/App/controllers/ScreenCtrl.js
@@ -3,34 +3,36 @@ var screenModule = angular.module('Screen', []);
 
 screenModule.controller('ScreenController', ['$scope', 'LayoutScreenService', '$routeParams', 'appConfig', '$timeout',
     function ($scope, LayoutScreenService, $routeParams, appConfig, $timeout) {
- 
         LayoutScreenService.getScreen($routeParams.id)
-            .error(function (data, status, headers, config) {
-
-            })
-            .success(function (screenData, status, headers, config) {
-                if (screenData != null) { //I dagsläget returnerar servern en 200 även om id inte finns i databasen men data är däremot null
-                    $scope.screen = screenData;
-
-                    LayoutScreenService.getPagesWithScreenId($scope.screen.id)
-                        .success(function (pagesData) {
-                        if(pagesData != null){
-                            setPages(pagesData);
-                        } else {
-                            console.log("Verkar inte få något bra svar från servern, LayoutScreenService.getPagesWithScreenId id=" + $scope.screen.id +" är null");
-                        }
-                    });
-                } else {
-                    console.log("Verkar inte få något bra svar från servern, är databasen seedad och uppe? Annars kanske inte id:et finns")
-                }
-            });
+        .error(function (data, status, headers, config) {})
+        .success(function (screenData, status, headers, config) {
+            if (screenData != null) { //I dagsläget returnerar servern en 200 även om id inte finns i databasen men data är däremot null
+                $scope.screen = screenData;
+                LayoutScreenService.getPagesWithScreenId($scope.screen.id)
+                .success(function (pagesData) {
+                    if(pagesData != null){
+                        setPages(pagesData);
+                    }
+                    else {
+                        console.log("Verkar inte få något bra svar från servern, LayoutScreenService.getPagesWithScreenId id=" + $scope.screen.id +" är null");
+                    }
+                });
+            }
+            else{
+                console.log("Verkar inte få något bra svar från servern, är databasen seedad och uppe? Annars kanske inte id:et finns")
+            }
+        });
 
         function setPages(pagesData) {
             $scope.pages = pagesData;
             var current = 0;
+            nextSlide($scope.pages[current]);
+            current = (current + 1 >= $scope.pages.length) ? 0 : current + 1;
 
             var sliderFunc = function () {
                 timer = $timeout(function () {
+                    $scope.page = null;
+                    $scope.partials = null;
                     LayoutScreenService.getPage($scope.pages[current].id).success(function (data) {
                         nextSlide(data);
                         current = (current + 1 >= $scope.pages.length) ? 0 : current + 1;
@@ -47,7 +49,7 @@ screenModule.controller('ScreenController', ['$scope', 'LayoutScreenService', '$
 
         function nextSlide(page) {
             $scope.page = page;
-            var templateName = page.template.fileName == null ? "default_template" : page.template.fileName;
+            var templateName = page.template.fileName == null ? "default.html" : page.template.fileName;
             var templateUrl = appConfig.templateUrlRoot + templateName;
             $scope.pageTemplate = templateUrl;
 
